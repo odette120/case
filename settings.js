a33c0d61728ac730cbf6a0a9c516f30c9ed5556c
odette120 <110310656+odette120@users.noreply.github.com>
2023-04-02 22:48:11 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,25 +7,25 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'TICSCaxXsHykqSh',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "WhatsApp Bot"
+global.namaowner = "Irmaofc"
 global.packname = namabot
 global.author = namaowner
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['62895366943070']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
 global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
 global.mess = {
-    done: 'Done ✅',
-    wait: '_⌛ Sedang di proses_',
+    done: 'Sussces🌝💫',
+    wait: '_☺️Harap_Menunggu',
     admin: '_Fitur Khusus Admin Group_',
     botNotAdmin: '_Jadikan Bot Sebagai Admin Terlebih Dahulu_',
     owner: '_Fitur Khusus Owner Bot_',
